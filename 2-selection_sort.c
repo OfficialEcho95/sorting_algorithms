53c3773bc3748b05c034a22de634b613b82b7e4a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-24 11:07:54 +0000

    Initial

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,25 +19,24 @@ void swap(int *a, int *b)
  * @array: array to be sorted
  * size: The size of the array
  */
-void selection_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size) 
 {
-	size_t i, j, min_idx;
-
-    for(i = 0; i < size - 1; i++)
+    for (size_t i = 0; i < size - 1; i++) 
     {
-        min_idx = i;
-
-        for (j = i+1; j < size; j++)
+        size_t min_idx = i;
+        for (size_t j = i + 1; j < size; j++) 
         {
-            if (array[j] < array[min_idx])
+            if (array[j] < array[min_idx]) 
             {
                 min_idx = j;
             }
-	if(min_idx != i)
-	{
-		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
         }
-	}
+        if (min_idx != i)
+        {
+            int temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);
+        }
     }
 }
