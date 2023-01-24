8b89f961c31b741f4c214636b84cf343e7ead177
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-24 10:42:35 +0000

    Initial

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap- the function that swaps variables
+ * @a: the first integer
+ * @b: second integer
+ */
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/**
+ * selection_sort- the sort function
+ * @array: array to be sorted
+ * size: The size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+    for(i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+
+        for (j = i+1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+            {
+                min_idx = j;
+            }
+            if(min_idx != i)
+                swap(&array[min_idx], &array[i]);
+        }
+    }
+}
